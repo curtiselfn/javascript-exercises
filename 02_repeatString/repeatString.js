a4d2ea1a8b04cdb9fcc07f756f8f5d14fc0f40e7
curtiselfn <curtis.elfn@gmail.com>
2025-04-06 15:30:50 +0700

    done with exercise 2 with help of ai to explain

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(words, manyTimes) {
+    let result = ""
+    if (manyTimes < 0) {
+        return "ERROR"
+    }
+    for (i = 0; i < manyTimes; i++){
+        result += words;
+    }
+    return result
 };
 
 // Do not edit below this line
