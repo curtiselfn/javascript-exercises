57d012214a1123712fd4413da68c5d5425445e64
curtiselfn <curtis.elfn@gmail.com>
2025-04-08 23:25:49 +0800

    done with 05 with ai (ofc)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    if (firstNumber < 0 || lastNumber < 0 ||
+         firstNumber === String || lastNumber === String || 
+         typeof firstNumber !== 'number' || 
+         typeof lastNumber !== 'number' || 
+         !Number.isInteger(firstNumber) ||
+         !Number.isInteger(lastNumber) ){
+        return "ERROR";
+    }
+    if (firstNumber > lastNumber) {
+        [firstNumber, lastNumber] = [lastNumber, firstNumber];
+    }
+    
+    let finalSum = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        finalSum += i;
+    }
 
+    return finalSum;
 };
 
 // Do not edit below this line
