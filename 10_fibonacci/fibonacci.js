e1ada71b58a8867e216e87655c40f10cf93825ea
curtiselfn <curtis.elfn@gmail.com>
2025-04-13 00:37:42 +0800

    will ask for pseudocode from now on

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number === 0 || number === "0") {
+        return 0;
+    }
 
+    if (number === 1) {
+        return 1;
+    }
+
+    let a = 0;
+    let b = 1;
+    let next;
+
+    for (let i = 2; i <= number; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
