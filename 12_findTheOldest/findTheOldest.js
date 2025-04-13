1d52757ff66663a9b46afd56545f55306d6ded93
curtiselfn <curtis.elfn@gmail.com>
2025-04-13 20:00:44 +0800

    speedrunning with AI

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const getAge = (person) => {
+            const deathYear = person.yearOfDeath || new Date().getFullYear();
+            return deathYear - person.yearOfBirth;
+        };
+        const oldestAge = getAge(oldest);
+        const currentAge = getAge(current);
 
+        return currentAge > oldestAge ? current : oldest;
+    }
+
+    )
 };
 
 // Do not edit below this line
